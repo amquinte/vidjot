2a4415d90dce96c4a23228fce6b68c368a5a570c
Anthony Quintero-Quiroga <anthony.quiroga@gmail.com>
2017-11-08 15:39:58 -0800

    Added handlebars and created some views for index and about page

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
+//const is a ES6 feature
 const app = express();
-const port = 5000;
+const port = 5500;
 
+// Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//Index route
+app.get('/', (req, res) => {
+    const title = 'Welcome1';
+    res.render('index', {
+        title: title
+    });
+});
+
+//About route
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+//Using arror function notation =>
 app.listen(port, () => {
-    console.log(`Started server on ${port}`)
+    console.log(`Started server on ${port}`) //Use backticks ` for template literals ${var}
 });
