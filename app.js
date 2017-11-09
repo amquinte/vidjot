ba1963df088e736317f45548a9b710cf507aedd2
Anthony Quintero-Quiroga <anthony.quiroga@gmail.com>
2017-11-08 16:56:56 -0800

    Added moongose and connected to mlab db.

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 //const is a ES6 feature
 const app = express();
+
+//Map global promise
+mongoose.Promise = global.Promise;
+//Connect to mongoose
+mongoose.connect('mongodb://amquinte:test@ds153015.mlab.com:53015/vidjot', {
+    useMongoClient: true
+})
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
+
 const port = 5500;
 
 // Handlebars middleware
