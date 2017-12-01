3ce0e77aafd18e87b6572f3fb19eeb2f7d9d6032
Anthony Quintero-Quiroga <anthony.quiroga@gmail.com>
2017-11-30 17:14:17 -0800

    Added logout functionality

--- routes/users.js
+++ routes/users.js
@@ -100,4 +100,11 @@ router.post('/register', (req, res) => {
 
 })
 
+//Logout user
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'Successfully logged out');
+    res.redirect('/users/login');
+});
+
 module.exports = router;
